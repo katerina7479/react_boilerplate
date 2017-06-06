10693e3d7a0fd8552c2ed4b0d3c266ba373d05a5
Katerina Hanson <katerina.hanson@stridehealth.com>
2017-06-06 12:04:09 -0700

    Boilerplate app

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Greeter = require('Greeter')
+
+// Call it
+ReactDOM.render(
+  <Greeter />,
+  document.getElementById('app')
+);
